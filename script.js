203bf4ebd3f241fee863346b1fb0ca290ac217dc
andrasszentgyorgyi <181062883+andrasszentgyorgyi@users.noreply.github.com>
2026-02-22 19:57:57 +0100

    Created basic layout. Hard-coded some books for easier testing. Script now creates card elements for each book in the array and appends them to HTML. Basic styling.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const cardContainer = document.getElementById("cards-container");
 
 function Book(title, author, pages) {
     this.title = title;
@@ -7,12 +8,21 @@ function Book(title, author, pages) {
     this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary(title, author, pages){
+function addBookToLibrary(title, author, pages) {
     myLibrary.push(new Book(title, author, pages));
 }
 
-function libraryCatalog(){
-    for (let i = 0; i<myLibrary.length; i++){
+function displayBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
         console.log(myLibrary.at(i));
+        var div = document.createElement("div");
+        div.innerHTML = myLibrary.at(i).title;
+        div.classList.add("book-card");
+        cardContainer.appendChild(div);
     }
 }
+
+addBookToLibrary("Skulduggery Pleasant", "Derek Landy", 384);
+addBookToLibrary("Norwegian Wood", "Haruki Murakamai", 390);
+addBookToLibrary("Just Kids", "Patti Smith", 320);
+
