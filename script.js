2a40953a4e2f60677313930a4fc14953e0403094
andrasszentgyorgyi <181062883+andrasszentgyorgyi@users.noreply.github.com>
2026-02-24 16:32:10 +0100

    User can now mark a book as read, or currently reading- this changes the background color of the card.

--- script.js
+++ script.js
@@ -16,10 +16,26 @@ function addBookToLibrary(title, author, pages) {
 function displayBooks() {
     for (let i = 0; i < myLibrary.length; i++) {
         console.log(myLibrary.at(i));
-        var card = bookTemplate.content.cloneNode(true);
-        var title = card.querySelector(".book-title");
-        var author = card.querySelector(".book-author")
-        var pages = card.querySelector(".book-pages");
+        let card = bookTemplate.content.cloneNode(true);
+        let title = card.querySelector(".book-title");
+        let author = card.querySelector(".book-author")
+        let pages = card.querySelector(".book-pages");
+        let readBtn = card.querySelector(".read-btn");
+        let readingBtn = card.querySelector(".reading-btn");
+        let removeBtn = card.querySelector(".remove-btn");
+        let bookCard = card.querySelector(".book-card");
+        readBtn.addEventListener("click", function() {
+            if(bookCard.classList.contains("status-reading")){
+                bookCard.classList.remove("status-reading")
+            }
+            bookCard.classList.toggle("status-read");
+        });
+        readingBtn.addEventListener("click", function() {
+            if(bookCard.classList.contains("status-read")){
+                bookCard.classList.remove("status-read")
+            }
+            bookCard.classList.toggle("status-reading")
+        });
         title.textContent = myLibrary.at(i).title;
         author.textContent = "by: " + myLibrary.at(i).author;
         pages.textContent = myLibrary.at(i).pages + " pages";
@@ -28,6 +44,6 @@ function displayBooks() {
 }
 
 addBookToLibrary("Skulduggery Pleasant", "Derek Landy", 384);
-addBookToLibrary("Norwegian Wood", "Haruki Murakamai", 390);
+addBookToLibrary("Norwegian Wood", "Haruki Murakami", 390);
 addBookToLibrary("Just Kids", "Patti Smith", 320);
 
